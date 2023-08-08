b11bd87d2ca129da027fef3c53a66f9669be2dd8
AderHenrique <adercontas@gmail.com>
2023-08-08 11:24:48 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Exercicio multiplicação
+const mutiplicacao = (num1, num2) => num1 * num2;
+console.log(mutiplicacao(10, 20));
+// Exercicio saudação
+const dizOi = (nome) => {
+    return (`Olà ${nome}, como você está ?`);
+};
+console.log(dizOi("Ader"));
---
+++ main.ts
@@ -0,0 +1,14 @@
+// Exercicio multiplicação
+
+const mutiplicacao = (num1: number, num2:number): number => num1 * num2
+
+console.log(mutiplicacao(10, 20))
+
+// Exercicio saudação
+
+const dizOi = (nome: string):string => {
+   return (`Olà ${nome}, como você está ?`)
+}
+
+console.log(dizOi("Ader"))
+
